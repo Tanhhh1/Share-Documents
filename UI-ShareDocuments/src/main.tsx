c833f56bff380d650c13dfef4f8de892522d2d0f
Tanhhh1 <anhtranduy25@gmail.com>
2026-08-09 01:17:04 +0700

    Initialize project structure

--- UI-ShareDocuments/src/main.tsx
+++ UI-ShareDocuments/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+import { store } from "@/app/store/store";
+import QueryProvider from "@/app/providers/query_provider";
+import ToastProvider from "@/app/providers/toast_provider";
+import "@/common/api/interceptor";
+import "./styles/common.css"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <QueryProvider>
+                <App />
+                <ToastProvider />
+            </QueryProvider>
+        </Provider>
+    </BrowserRouter>
+);
